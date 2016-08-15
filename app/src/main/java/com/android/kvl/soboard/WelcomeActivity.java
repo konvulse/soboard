e31ed8700be580061953d5386d080f7b9f025e94
Kevin Van Leer <kevin.vanleer@gmail.com>
2016-08-14 21:20:48 -0500

    Added everything in source directory
    A very minimal set of functionality established.

---
+++ app/src/main/java/com/android/kvl/soboard/WelcomeActivity.java
@@ -0,0 +1,137 @@
+package com.android.kvl.soboard;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "WelcomeActivity";
+
+    private static final int PICK_IMAGE = 1;
+    private static final int WRITE_EXTERNAL_STORAGE = 2;
+
+    ListView boardingPassListView;
+    static final String BOARDING_PASS_EXTRA = "com.android.kvl.soboard.boarding_pass";
+
+    //ArrayAdapter<> images;
+    final Activity context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+                int permissionCheckStorage = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                int permissionCheckSettings = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_SETTINGS);
+
+                if (permissionCheckStorage != PackageManager.PERMISSION_GRANTED || permissionCheckSettings != PackageManager.PERMISSION_GRANTED ) {
+                    Log.d(LOG_TAG, "requesting permissions to access image gallery");
+                    ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,  Manifest.permission.WRITE_SETTINGS}, WRITE_EXTERNAL_STORAGE);
+                } else {
+                    Log.d(LOG_TAG, "permission already granted");
+                    getImage();
+                }
+
+            }
+        });
+
+        boardingPassListView = (ListView) findViewById(R.id.boardingPassListView);
+    }
+
+    void getImage() {
+        Log.d(LOG_TAG, "The user will now select an image to view");
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+
+            case WRITE_EXTERNAL_STORAGE:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Log.d(LOG_TAG, "WRITE_EXTERNAL_STORAGE permission granted");
+                    getImage();
+                } else {
+                    Log.d(LOG_TAG, "WRITE_EXTERNAL_STORAGE permission denied");
+                }
+
+                break;
+
+            default:
+                break;
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        switch (requestCode) {
+            case PICK_IMAGE:
+                //images.
+                //boardingPassListView.
+                if(data == null) {
+                    return;
+                }
+/*
+                Settings.System.putInt(context.getContentResolver(),
+                        Settings.System.SCREEN_BRIGHTNESS, 20);
+
+                WindowManager.LayoutParams lp = getWindow().getAttributes();
+                lp.screenBrightness =0.2f;// 100 / 100.0f;
+                getWindow().setAttributes(lp);
+*/
+                Intent displayImage = new Intent(this, BoardingPassActivity.class);
+                displayImage.putExtra(BOARDING_PASS_EXTRA, data);
+                startActivity(displayImage);
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
